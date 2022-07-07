d7df08cbd95d712493a0615b60cff10007578d50
Hyunsdb <hyunsdb@naver.com>
2022-07-08 05:55:48 +0900

    Initial commit

---
+++ src/test/java/com/triple/mileage/MileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
